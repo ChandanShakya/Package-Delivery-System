52e763f2d427fdc5cd8c4dc60fcf5edf930dfc20
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-05-22 09:13:13 +0545

    Added delivery rider's detail to the user table

--- New/php/user_package_records_content.php
+++ New/php/user_package_records_content.php
@@ -15,12 +15,40 @@
                         <th>Pickup Address</th>
                         <th>Contact no.</th>
                         <th>Description</th>
+                        <th>Delivery Rider's Name</th>
+                        <th>Delivery Rider's Contact No</th>
                         <th>Delivery Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    $sql = "SELECT pd.date_received, pd.date_delivered,pd.description, pd.order_code, rd.name AS receiver_name, ad.address AS receiver_address, ad2.address AS pickup_address, rd.phone_no AS receiver_phone, ds.status_name AS delivery_status FROM package_details pd JOIN account_details ad1 ON pd.account_id = ad1.account_id JOIN receiver_details rd ON pd.receiver_id = rd.receiver_id JOIN address_details ad ON pd.order_code = ad.order_code AND ad.address_type_id = 2 JOIN address_details ad2 ON pd.order_code = ad2.order_code AND ad2.address_type_id = 1 JOIN delivery_status ds ON pd.delivery_status_id = ds.status_id WHERE pd.account_id = :account_id";
+                    $sql = "SELECT
+                    pd.date_received,
+                    pd.date_delivered,
+                    pd.order_code,
+                    rd.name AS receiver_name,
+                    ad.address AS receiver_address,
+                    ad2.address AS pickup_address,
+                    rd.phone_no AS receiver_phone,
+                    pd.description,
+                    ds.status_name AS delivery_status,
+                    (SELECT ad.name FROM account_details ad WHERE pd.delivery_rider_id = ad.account_id) AS delivery_rider_name,
+                    (SELECT ad.phone_no FROM account_details ad WHERE pd.delivery_rider_id = ad.account_id) AS delivery_rider_phone
+                  FROM
+                    package_details pd
+                  JOIN
+                    account_details ad1 ON pd.account_id = ad1.account_id
+                  JOIN
+                    receiver_details rd ON pd.receiver_id = rd.receiver_id
+                  JOIN
+                    address_details ad ON pd.order_code = ad.order_code AND ad.address_type_id = 2
+                  JOIN
+                    address_details ad2 ON pd.order_code = ad2.order_code AND ad2.address_type_id = 1
+                  JOIN
+                    delivery_status ds ON pd.delivery_status_id = ds.status_id
+                  WHERE
+                    pd.account_id = :account_id;
+                  ";
                     $stmt = $pdo->prepare($sql);
                     $stmt->bindParam(':account_id', $accountId, PDO::PARAM_INT);
                     $stmt->execute();
@@ -37,6 +65,8 @@
                         echo '<td>' . $package['pickup_address'] . '</td>';
                         echo '<td>' . $package['receiver_phone'] . '</td>';
                         echo '<td>' . $package['description'] . '</td>';
+                        echo '<td>' . $package['delivery_rider_name'] . '</td>';
+                        echo '<td>' . $package['delivery_rider_phone'] . '</td>';
                         echo '<td>' . $package['delivery_status'] . '</td>';
                         echo '</tr>';
                     }
